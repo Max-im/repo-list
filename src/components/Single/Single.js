6b6bcc13ef0ef919a33fe12bec173977e4c9a2f7
Max Pogidaev <pogidaevmo@gmail.com>
2019-11-24 23:03:12 +0200

    fix single page

--- src/components/Single/Single.js
+++ src/components/Single/Single.js
@@ -1,53 +1,55 @@
-import React, { Component } from 'react';
-import './Single.css';
+import React, { Component } from "react";
+import "./Single.css";
 
 class Single extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       arr: []
-    }
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const params = this.props.params.id;
 
-    const url = `https://api.github.com/repos/defunkt/${params}/contributors`;
-    fetch(url, {method: 'GET'})
+    const url = `https://api.github.com/repos/max-im/${params}/contributors`;
+    fetch(url, { method: "GET" })
       .then(raw => raw.json())
-      .then(json => this.setState({arr: json}))
-      
+      .then(json => this.setState({ arr: json }));
   }
   render() {
     return (
       <div className="single container">
-      <h2>Contributors page of the 
-        <span className="single__projectName">{this.props.params.id}</span>
-        project
-      </h2>
-        {
-          this.state.arr.map((item, index) => {
-            return(
-              <div className="listItem" key={item.id}>
-                <div className="listItem__content">
-                  <span className="listItem__number">{index + 1}</span>
-                  
-                  <div className="listItem__autorInfo">
-                    <img alt={item.login} className="listItem__img" src={item.avatar_url} />
-                    <span>{item.login}</span>
-                  </div>
-                </div>
-                
-                <a target="_blank" href={item.html_url}>link</a>
+        <h2>
+          Contributors page of the
+          <span className="single__projectName">{this.props.params.id}</span>
+          project
+        </h2>
+        {this.state.arr.map((item, index) => {
+          return (
+            <div className="listItem" key={item.id}>
+              <div className="listItem__content">
+                <span className="listItem__number">{index + 1}</span>
 
+                <div className="listItem__autorInfo">
+                  <img
+                    alt={item.login}
+                    className="listItem__img"
+                    src={item.avatar_url}
+                  />
+                  <span>{item.login}</span>
+                </div>
               </div>
-            )
-          })
-        }
+
+              <a target="_blank" href={item.html_url}>
+                link
+              </a>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
-
 
 export default Single;
