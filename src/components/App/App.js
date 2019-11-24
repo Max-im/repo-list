6adc3b725e94c3c757fb5745739d9c8d6eeb0e89
Max Pogidaev <pogidaevmo@gmail.com>
2019-11-24 22:49:40 +0200

    display my projects

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
-import { fetchData } from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
+import { fetchData } from "../../actions";
 
-import HeaderComponent from '../HeaderComponent/HeaderComponent';
+import HeaderComponent from "../HeaderComponent/HeaderComponent";
 
 class App extends Component {
-  
-  componentDidMount(){
-      const target = 'defunkt';
-      const url = `https://api.github.com/users/${target}/repos`;
-      fetch(url, {method: 'GET'})
-        .then(raw => raw.json())
-        .then(json => this.props.fetchData(json));
+  componentDidMount() {
+    const target = "max-im";
+    const url = `https://api.github.com/users/${target}/repos`;
+    fetch(url, { method: "GET" })
+      .then(raw => raw.json())
+      .then(json => this.props.fetchData(json));
   }
 
   render() {
     return (
       <div className="app">
         <HeaderComponent />
-        <main className="main">
-          {this.props.children}
-        </main>
+        <main className="main">{this.props.children}</main>
       </div>
     );
   }
 }
-
 
 export default connect(null, { fetchData })(App);
